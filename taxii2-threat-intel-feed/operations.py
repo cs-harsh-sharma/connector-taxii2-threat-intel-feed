4206c2dcc8fa2144c29a9e5b3e0128644addd359
cs-harsh-sharma <sharmah@fortinet.com>
2025-06-05 15:31:12 +0530

    Addressed review comments

--- taxii2-threat-intel-feed/operations.py
+++ taxii2-threat-intel-feed/operations.py
@@ -10,11 +10,11 @@ import requests
 import uuid
 from connectors.cyops_utilities.builtins import create_file_from_string
 from connectors.core.connector import get_logger, ConnectorError
-from connectors.cyops_utilities.files import get_ingestion_base_dir
 from datetime import datetime
 
 try:
     from integrations.crudhub import trigger_ingest_playbook
+    from connectors.cyops_utilities.files import get_ingestion_base_dir
 except:
     # ignore. lower FSR version
     pass
--- taxii2-threat-intel-feed/release_notes.md
+++ taxii2-threat-intel-feed/release_notes.md
@@ -1,7 +1,7 @@
 #### What's Improved
 
-- Added a new config parameter 'Headers'.
-- Added a new parameter "Headers" in following actions:
+- Added a new configuration parameter `Headers`.
+- Added a new parameter `Headers` in following actions:
   - Get Collections
   - Fetch Indicators
-  - Download Indicators
+- Added new action `Download Indicators`, note that this action is added for FCP (Not visible in FortiSOAR).
